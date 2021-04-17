e997836a95675c85ed21e1c7ad3dfc712ab2f8d6
Nitesh Bagle <niteshbagle43@gmail.com>
2021-04-17 19:52:41 +0530

    expense date component added

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+    const { expenseDate } = props;
+    const month = expenseDate.toLocaleString('en-US', { month: 'long'});
+    const day = expenseDate.toLocaleString('en-US', { day: '2-digit' });
+    const year = expenseDate.getFullYear();
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__day">{year}</div>
+            <div className="expense-date__year">{day}</div>
+        </div>
+    );
+}
+
+export default ExpenseDate;
